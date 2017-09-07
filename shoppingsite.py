15adfe2372c8a5c1a832b0b714eb7f5a20904ac3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-06 18:03:10 -0700

    added session

--- shoppingsite.py
+++ shoppingsite.py
@@ -7,7 +7,7 @@ Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print melon
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -97,10 +97,19 @@ def add_to_cart(melon_id):
     #   dictionary keyed to the string "cart") if not
     # - check if the desired melon id is the cart, and if not, put it in
     # - increment the count for that melon id by 1
-    # - flash a success message
-    # - redirect the user to the cart page
+    # - flash a success messag
+    # - redirect the user to the cart paget
 
-    return "Oops! This needs to be implemented!"
+    if 'cart' not in session:
+        session['cart'] = {}
+
+    if melon_id in session['cart']:
+        session['cart'][melon_id] += 1
+    else:
+        session['cart'][melon_id] = 1
+    print session['cart']
+    return redirect('/cart')
+
 
 
 @app.route("/login", methods=["GET"])
